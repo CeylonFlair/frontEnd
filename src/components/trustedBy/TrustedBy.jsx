ed39303950c9b1c2332c4ae5f9d90123dbf48c40
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2025-06-01 00:58:28 +0530

    added all pages

---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./TrustedBy.scss";
+
+const TrustedBy = () => {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <div className="title">
+          <span>Our Values</span>
+        </div>
+        
+        <div className="logos">
+      <img src="../../../public/img/1.png"/>
+      <img src="../../../public/img/2.png" />
+      <img src="../../../public/img/3.png"/>
+      <img src="../../../public/img/4.png"/>
+    </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
-import "./Home.scss"
+import React from "react";
+import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
+import ProjectCard from "../../components/projectCard/ProjectCard";
+import { cards, projects } from "../../data";
 
-const Home = () => {
+function Home() {
   return (
-    <div className='home'>Home</div>
-  )
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
+      
+      
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+            <h1>
+              Introducing <i>CeylonFlair Pro</i>
+            </h1>
+            <h1>More tools. More reach. More flair
+            </h1>
+            <p>
+              Upgrade to a curated seller experience designed to grow your creative business
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Get discovered faster with priority placement on the homepage and search results
+            </div>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Showcase your credibility with a Verified Artisan badge proudly displayed on your profil
+            </div>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Enjoy one-on-one support from a dedicated success manager for guidance and growth
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Unlock exclusive seller tools, premium listing features, and seasonal marketing boosts
+            </div>
+            <button>Join the Beta Program</button>
+          </div>
+          <div className="item">
+            <img
+              src="../../../public/img/promotion.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} card={card} />
+        ))}
+      </Slide>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
