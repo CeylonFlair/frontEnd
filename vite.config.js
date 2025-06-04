eeb6db360f3ad9516ec1cf2446540c73322d3bb6
Apisathan Sivananthanathan <apisathan2000@gmail.com>
2025-06-04 21:52:15 +0530

    added host in vite configs

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
   // Added to support payment gateway proxy
   server: {
     proxy: {
-      '/api': process.env.VITE_API_BASE_URL
-    }
-  }
-})
+      "/api": process.env.VITE_API_BASE_URL,
+    },
+  },
+  preview: {
+    allowedHosts: ["ceylonflair-frontend-56c8eda4af29.herokuapp.com"],
+  },
+});
